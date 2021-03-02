d8d6364f47270bb33bfb1bdbfe83ce36c70dcbfe
shyam <shyam.burgula@gmail.com>
2021-03-02 14:57:31 +0530

    register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './views/login/login.component';
+import { ProductListComponent } from './views/product-list/product-list.component';
 import { RegisterComponent } from './views/register/register.component';
 
 const routes: Routes = [
   {
     path:'',
-    redirectTo:'/register',
+    redirectTo:'/products',
     pathMatch:'full'
   },
   {
@@ -16,6 +17,10 @@ const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent
+  },
+  {
+    path: 'products',
+    component: ProductListComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './views/login/login.component';
 import { RegisterComponent } from './views/register/register.component';
+import { ProductListComponent } from './views/product-list/product-list.component';
+import { ProfileComponent } from './views/profile/profile.component';
+import { OrdersComponent } from './views/orders/orders.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    ProductListComponent,
+    ProfileComponent,
+    OrdersComponent
   ],
   imports: [
     BrowserModule,
